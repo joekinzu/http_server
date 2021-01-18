72e014479a81e6cf7442c04372efe07d50dc92ee
joekinzu <34208764+joekinzu@users.noreply.github.com>
2021-01-18 20:07:29 +0400

    Update httpd.py

--- httpd.py
+++ httpd.py
@@ -1,31 +1,33 @@
 import socket
 import os
-
 HOST, PORT = 'localhost', 80
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
+
 print(f'Serving HTTP on port {PORT} ...')
 while True:
     client_connection, client_address = listen_socket.accept()
     request_data = client_connection.recv(1024)
-    # file_size = os.path.getsize('./httptest/160313.jpg')
-    # print(file_size)
     try:
     	method, url, ver = (request_data.decode('utf-8').split('\n')[0]).split()
-    	print(method+'--'+url+'--'+ver)
-    	# x = [os.path.join('httptest', fn) for fn in next(os.walk('httptest'))[2]]
-    	# print(next(os.walk('httptest')))
+    	print(ver+' '+method+' request: '+url)
     	if method in ('GET', 'HEAD'):
     		http_response = b'HTTP/1.1 200 OK\n\nOK!\n'
     		if len(url.split('.')[-1]) < len(url):	
     			file_size = os.path.getsize('.'+url)
-    			# content_type = mimetypes.guess_type('./httptest/160313.jpg')[0]
-    			# print(content_type, file_size)
     			with open('.'+url, 'rb') as file:
     				http_response = file.read(file_size)
+    		else:
+    			x = next(os.walk('.'+url))[2]
+    			y = next(os.walk('.'+url))[1] + x
+    			html='<html>\n'+'<body>\n'+'<ul>\n'
+    			for z in y:
+    				html = html + '	<li><a href="'+url+'/'+z+'">' + z + '</a></li>\n'
+    			shtml='HTTP/1.1 200 OK\n\n'+html+'<ul>\n'+'</body>\n'+'</html>\n'+'\n'
+    			http_response = shtml.encode()
     		client_connection.sendall(http_response)
     	else:
     		http_response = b'HTTP/1.1 400 BAD\n\nNOT OK!\n'
